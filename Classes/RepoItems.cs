d4b3b2200ee27d92fef2c09ffbb45b6c6262b26e
qwel0rd <secondofall22@rambler.ru>
2025-10-06 10:24:53 +0500

    Добавьте файлы проекта.

---
+++ Classes/RepoItems.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Пешин.Classes
+{
+    internal class RepoItems
+    {
+    }
+}
