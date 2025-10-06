a0b45b8530ac21d88317afa1b16d09d10243bd33
qwel0rd <secondofall22@rambler.ru>
2025-10-06 11:31:21 +0500

    Программирование пользовательского элемента и окна MainWindow.  Пешин

---
+++ Elements/item.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+
+namespace Shop_Пешин.Elements
+{
+    public partial class Item : UserControl
+    {
+        public Item()
+        {
+            InitializeComponent();
+        }
+
+        // Этот метод должен быть обязательно!
+        public void SetData(string name, int price, string characteristic)
+        {
+            tb_Name.Content = name;
+            tb_Price.Content = $"Цена: {price} руб.";
+            tb_Characteristic.Content = characteristic;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using System.Xml.Linq;
 
 namespace Shop_Пешин
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            LoadItems();
+        }
+
+        private void LoadItems()
+        {
+            var items = Classes.RepoItems.AllItems();
+
+            foreach (var item in items)
+            {
+                if (item is Classes.Children childrenItem)
+                {
+                    var userControl = new Elements.Item();
+                    userControl.SetData(childrenItem.Name, childrenItem.Price, $"Возраст: {childrenItem.Age}+");
+                    parent.Children.Add(userControl);
+                }
+                else if (item is Classes.Sport sportItem)
+                {
+                    var userControl = new Elements.Item();
+                    userControl.SetData(sportItem.Name, sportItem.Price, $"Размер: {sportItem.Size}");
+                    parent.Children.Add(userControl);
+                }
+            }
         }
     }
 }
